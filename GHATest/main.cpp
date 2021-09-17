7f2f5b8df19dddcfc8df7132f726f6d3e5eb3ca0
Peter Sjögren <peter.sjogren@pskonsult.se>
2021-09-17 14:08:28 +0200

    Initial Commit

---
+++ GHATest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GHATest
+//
+//  Created by Peter Sjögren on 2021-09-17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
